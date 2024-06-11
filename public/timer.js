35760c7737405f09d01f00a8ba17e3506c2cdfd4
Amit Arya <aryarithm@gmail.com>
2024-06-11 21:35:33 +0530

    Update timer.js
    code optimized

--- public/timer.js
+++ public/timer.js
@@ -1,105 +1,81 @@
-;(function(exports) {
+(function (exports) {
+  function Timer() {
+    this.endTime = 0;
+    this.timeLeft = 0;
+    this.intervalId = null;
+  }
 
-	function Timer() {
-		this.endTime = new Date().getTime();
-	}
+  Timer.prototype = {
+    setEndTime: function (time) {
+      this.endTime = new Date().getTime() + time;
+      this.updateTimeRemaining();
+      this.startTimer();
+    },
+    setEndTimeFromServer: function (time) {
+      this.endTime = time;
+      this.updateTimeRemaining();
+      this.startTimer();
+    },
+    updateTimeRemaining: function () {
+      this.timeLeft = Math.max(0, this.endTime - new Date().getTime());
+    },
+    formatTime: function () {
+      var time = this.timeLeft,
+        seconds = Math.floor((time / 1000) % 60),
+        minutes = Math.floor((time / (1000 * 60)) % 60),
+        hours = Math.floor((time / (1000 * 60 * 60)) % 24);
 
-	Timer.prototype = {
+      return (
+        (hours > 0 ? (hours < 10 ? "0" + hours : hours) + ":" : "") +
+        (minutes < 10 ? "0" + minutes : minutes) +
+        ":" +
+        (seconds < 10 ? "0" + seconds : seconds)
+      );
+    },
+    startTimer: function () {
+      var self = this;
+      if (this.intervalId) {
+        clearInterval(this.intervalId);
+      }
+      this.intervalId = setInterval(function () {
+        self.updateTimeRemaining();
+        $("#timer").text(self.formatTime());
+        if (self.timeLeft <= 0) {
+          clearInterval(self.intervalId);
+          self.showButtons();
+        }
+      }, 1000);
+    },
+    showButtons: function () {
+      $("#buttons").fadeIn(1000, function () {
+        $(".bid-btns").fadeIn(100);
+      });
+      $(".bid-btns").prop("disabled", false);
+    },
+  };
 
-		setEndTime: function(time) {
-			this.endTime = new Date().getTime() + time;
-		},
-		setEndTimeFromServer: function(time) {
-			this.endTime = time;
-			this.timeRemaining();
-		},
-		timeRemaining: function() {
-			this.timeLeft = this.endTime - new Date().getTime();
-		},
-		format: function() {
-			var time = this.timeLeft,
-				seconds = Math.floor((time / 1000) % 60),
-				minutes = Math.floor((time / (1000 * 60)) % 60),
-				hours = Math.floor((time / (1000 * 60 * 60)) % 24);
+  exports.Timer = Timer;
+})(typeof exports === "undefined" ? this : exports);
 
-			var formattedTime = "";
-			if (hours > 0 && hours < 10) {
-				formattedTime += "0" + hours + ":";
-			} else if (hours >= 10) {
-				formattedTime += hours + ":";
-			}
+(function () {
+  var timer = new Timer(),
+    socket = io.connect("http://localhost:8080");
 
-			if (seconds < 0 && minutes != 0) {
-			    minutes -= 0;
-			    seconds = 0;
-			}
-			if (minutes < 10) {
-				formattedTime += "0" + minutes + ":";
-			} else {
-				formattedTime += minutes + ":";
-			}
+  socket.on("currentEndTime", function (data) {
+    timer.setEndTimeFromServer(data.time);
+  });
 
-			if (seconds < 10) {
-				formattedTime += "0" + seconds;
-			} else {
-				formattedTime += seconds;
-			}
+  $(function () {
+    set = setInterval(function () {
+      $(".time").trigger("change");
+      clearInterval(set);
+    }, 100);
 
-			return formattedTime;
-		}
-	};
-
-	exports.Timer = Timer;
-
-})(typeof exports === 'undefined' ? this : exports);
-
-;(function() {
-
-	var timer = new Timer(),
-		socket = io.connect('http://localhost:8080');
-	
-	socket.on('currentEndTime', function (data) {
-		//this is the full date time in ms.
-		timer.setEndTimeFromServer(data.time);
-	});
-
-	$(function() {
-
-		//AA[alert user about the auction start time]
-		// var millisecondVal = $('.time').text() * 60 * 1000;
-		// var minuteVal = millisecondVal / 60000;
-		// alert('Auction will start in '+minuteVal+' minutes. Please Wait...');
-		// console.log(timer.timeRemaining());
-
-		// var user="5/18/2017/6/50";
-		// var arrdt= user.split("/");
-		// var userdt = new Date(arrdt[2], arrdt[1] - 1, arrdt[0],arrdt[3],arrdt[4]);
-		// var currdt = new Date();
-		// if (userdt === currdt) {
-		// alert("time bhayo");
-		// }
-
-		//AA[set auction start time]
-		set = setInterval(function(){
-			$('.time').trigger('click');
-			clearInterval(set);
-		},20000);
-		//AA[clear interval once broadcasted]
-
-		$('.time').on('click', function(e) {
-			e.stopPropagation();
-			var time = $(this).text() * 60 * 1000;
-			timer.setEndTime(time);
-			timer.timeRemaining();
-			socket.emit('setTimer', { time: time });
-		});
-	});
-
-	setInterval(function() {
-		if (timer.timeLeft > 0) {
-			timer.timeRemaining();
-			$('#timer').text(timer.format());
-		}
-	},100);
-
+    $(".time").on("change", function (e) {
+      e.stopPropagation();
+      var time = $(this).text() * 1000;
+      timer.setEndTime(time);
+      socket.emit("setTimer", { time: time });
+    });
+  });
 })();
