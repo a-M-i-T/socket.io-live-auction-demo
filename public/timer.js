c67d8fd7134f36b3a1974162ad5a5c795de8ac6b
Amit Arya <aryarithm@gmail.com>
2024-06-11 22:05:55 +0530

    Update timer.js
    code optmized
    
    fix class error

--- public/timer.js
+++ public/timer.js
@@ -1,81 +1,88 @@
 ;(function (exports) {
-  function Timer() {
-    this.endTime = 0;
-    this.timeLeft = 0;
-    this.intervalId = null;
-  }
-
-  Timer.prototype = {
-    setEndTime: function (time) {
-      this.endTime = new Date().getTime() + time;
-      this.updateTimeRemaining();
-      this.startTimer();
-    },
-    setEndTimeFromServer: function (time) {
-      this.endTime = time;
-      this.updateTimeRemaining();
-      this.startTimer();
-    },
-    updateTimeRemaining: function () {
-      this.timeLeft = Math.max(0, this.endTime - new Date().getTime());
-    },
-    formatTime: function () {
-      var time = this.timeLeft,
-        seconds = Math.floor((time / 1000) % 60),
-        minutes = Math.floor((time / (1000 * 60)) % 60),
-        hours = Math.floor((time / (1000 * 60 * 60)) % 24);
-
-      return (
-        (hours > 0 ? (hours < 10 ? "0" + hours : hours) + ":" : "") +
-        (minutes < 10 ? "0" + minutes : minutes) +
-        ":" +
-        (seconds < 10 ? "0" + seconds : seconds)
-      );
-    },
-    startTimer: function () {
-      var self = this;
-      if (this.intervalId) {
-        clearInterval(this.intervalId);
-      }
-      this.intervalId = setInterval(function () {
-        self.updateTimeRemaining();
-        $("#timer").text(self.formatTime());
-        if (self.timeLeft <= 0) {
-          clearInterval(self.intervalId);
-          self.showButtons();
-        }
-      }, 1000);
-    },
-    showButtons: function () {
-      $("#buttons").fadeIn(1000, function () {
-        $(".bid-btns").fadeIn(100);
-      });
-      $(".bid-btns").prop("disabled", false);
-    },
-  };
-
-  exports.Timer = Timer;
-})(typeof exports === "undefined" ? this : exports);
-
-(function () {
-  var timer = new Timer(),
-    socket = io.connect("http://localhost:8080");
-
-  socket.on("currentEndTime", function (data) {
-    timer.setEndTimeFromServer(data.time);
-  });
-
-  $(function () {
-    set = setInterval(function () {
-      $(".time").trigger("change");
-      clearInterval(set);
-    }, 100);
-
-    $(".time").on("change", function (e) {
-      e.stopPropagation();
-      var time = $(this).text() * 1000;
-      timer.setEndTime(time);
-      socket.emit("setTimer", { time: time });
-    });
-  });
-})();
+	class Timer {
+	  constructor() {
+		this.endTime = 0;
+		this.timeLeft = 0;
+		this.intervalId = null;
+	  }
+  
+	  setEndTime(time) {
+		this.endTime = Date.now() + time;
+		this.updateTimeRemaining();
+		this.startTimer();
+	  }
+  
+	  setEndTimeFromServer(time) {
+		this.endTime = time;
+		this.updateTimeRemaining();
+		this.startTimer();
+	  }
+  
+	  updateTimeRemaining() {
+		this.timeLeft = Math.max(0, this.endTime - Date.now());
+	  }
+  
+	  formatTime() {
+		const time = this.timeLeft;
+		const seconds = Math.floor((time / 1000) % 60).toString().padStart(2, '0');
+		const minutes = Math.floor((time / (1000 * 60)) % 60).toString().padStart(2, '0');
+		const hours = Math.floor((time / (1000 * 60 * 60)) % 24).toString().padStart(2, '0');
+  
+		return `${hours > 0 ? hours + ':' : ''}${minutes}:${seconds}`;
+	  }
+  
+	  startTimer() {
+		if (this.intervalId) {
+		  clearInterval(this.intervalId);
+		}
+  
+		this.intervalId = setInterval(() => {
+		  this.updateTimeRemaining();
+		  document.getElementById('timer').textContent = this.formatTime();
+		  if (this.timeLeft <= 0) {
+			clearInterval(this.intervalId);
+			this.showButtons();
+		  }
+		}, 1000);
+	  }
+  
+	  showButtons() {
+		const buttons = document.getElementById('buttons');
+		buttons.style.display = 'block';
+		const bidBtns = document.querySelectorAll('.bid-btns');
+		bidBtns.forEach(btn => {
+		  btn.style.display = 'block';
+		  btn.disabled = false;
+		});
+	  }
+	}
+  
+	exports.Timer = Timer;
+  })(typeof exports === "undefined" ? this : exports);
+  
+  (function () {
+	const timer = new Timer();
+	const socket = io.connect("http://localhost:8080");
+  
+	socket.on("currentEndTime", (data) => {
+	  timer.setEndTimeFromServer(data.time);
+	});
+  
+	document.addEventListener('DOMContentLoaded', () => {
+	  const timeElements = document.querySelectorAll('.time');
+  
+	  setTimeout(() => {
+		timeElements.forEach(el => el.dispatchEvent(new Event('change')));
+	  }, 100);
+  
+	  timeElements.forEach(el => {
+		el.addEventListener('change', (e) => {
+		  e.stopPropagation();
+		  const time = parseInt(el.textContent, 10) * 1000;
+		  timer.setEndTime(time);
+		  socket.emit("setTimer", { time });
+		});
+	  });
+	});
+  })();
+  
